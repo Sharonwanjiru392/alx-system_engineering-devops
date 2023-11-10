0c01db409cdef2aa09cd8b1336a08ab1a756c2cd
Sharon <Sharonwanjiru392@gmail.com>
2023-11-09 19:53:24 -0800

    README.md

--- 0x18-webstack_monitoring/check_datadog_host_exists.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X GET "https://api.datadoghq.com/api/v1/hosts" \
--H "Accept: application/json" \
--H "DD-API-KEY: ${7c87085524cbf73af6ef78094cf84e2a}" \
--H "DD-APPLICATION-KEY: ${4dcebc9fb6dde1ed535330874c4ac9549a35a1fa}"
-
